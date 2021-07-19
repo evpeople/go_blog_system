cc74f83014389796f386989fbd05e5dedd55c1c2
hangcaihui <wangzhe177@bupt.edu.cn>
2021-07-19 15:47:05 +0800

    feat: not finished Route,because can't get TrueUrl for HandleFunc,can't run a func in another func with parameter transfer

--- route/route.go
+++ route/route.go
@@ -3,15 +3,36 @@ package route
 import (
 	"fmt"
 	"go_blog_system/route/internal/API"
+	"log"
 	"net/http"
+	"regexp"
 )
 
 func init() {
 
 	http.HandleFunc("/articles", API.ShowArticles)
 	http.HandleFunc("/article", API.AnalyzeMethod)
-	http.HandleFunc("/test", test)
+	http.HandleFunc("/test/", test)
+	HandleFunc("/api/", test, "[0-9]*")
 }
 func test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hehe")
+}
+
+func HandleFunc(url string, packHandler func(http.ResponseWriter, *http.Request, http.HandlerFunc), pattern string) {
+	trueURL, trueFunc := packHandler()
+	//todo 遇到的问题是无法直接运行包装函数，此函数必须要参数，而我如果有参数就不用包装器函数了
+
+	if matchd, err := regexp.MatchString(pattern, getTrueUrl(handler)); err != nil {
+		log.Default().Println(err)
+	} else if !matchd {
+		log.Default().Println("not matched")
+	} else {
+		http.DefaultServeMux.HandleFunc(url, handler)
+	}
+
+}
+
+func packHandler(w http.ResponseWriter, r *http.Request, handlerFunc http.HandlerFunc) (trueURL string, trueFunc http.HandlerFunc) {
+	return r.URL.Path, handlerFunc
 }
