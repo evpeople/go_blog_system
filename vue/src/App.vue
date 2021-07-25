42bbd75d0928f1623dc897c7b88bd9d5b7dd09ba
hangcaihui <wangzhe177@bupt.edu.cn>
2021-07-25 18:01:56 +0800

    build: Looks like I'd better start with basic JavaScript

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,23 +1,16 @@
+
 <template>
-  <img src="./assets/logo.png">
-  <div>
-    <p>
-      If Element Plus is successfully added to this project, you'll see an
-      <code v-text="'<el-button>'"></code>
-      below
-    </p>
-    <el-button type="primary">el-button</el-button>
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Main></Main>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from "@/components/Main";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import installElementPlus from './plugins/element'
+// import Vue from 'vue'
+// import App from './App.vue'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/lib/theme-chalk/index.css';
+//
+// Vue.config.productionTip = false
+//
+// new Vue({
+//     render: h => h(App),
+// }).$mount("#app").use(ElementPlus)
+import {createApp} from 'vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import App from './App.vue';
 
-Vue.config.productionTip = false
-
-new Vue({
-    render: h => h(App),
-}).$mount("#app")
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
